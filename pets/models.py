13580b0932b6fb356972b515510e78221c5e3b6c
frknozr <frknozr@gmail.com>
2015-08-20 14:21:52 +0300

    added new models

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Pet(models.Model):
+
+    GENDERS =[
+        ('m',"Male"),
+        ('f',"Female"),
+    ]
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    kind = models.CharField(max_length=50)
+    gender = models.CharField(max_length=1,choices=GENDERS)
+    is_barren = models.BooleanField(default=False)
+    image = models.URLField()
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    location = models.CharField(max_length=50)
+
+    pet = models.ForeignKey('Pet', related_name='posts',null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,null= True)
+
+class Message(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_messages')
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='receipt_messages')
+    subject = models.CharField(max_length=50)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False)
+
+
+
